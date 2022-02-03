99ace997ea2c9c8ec07915a4b8261b304daa45cb
koltvictor <kolts.email@gmail.com>
2022-02-03 15:53:37 -0800

    added welcome link styling

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 export default function Welcome() {
     return(
         <div className="welcome">
-            <Link to="/home">WELCOME</Link>
+            <Link to="/home" className="welcome">WELCOME</Link>
         </div>
     )
 }
