ef13178500aab42e7b611caa9e6def2b04090c0a
koltvictor <kolts.email@gmail.com>
2022-02-04 16:45:51 -0800

    commiting to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
  
   end
 
-  get '/hello', to: 'application#hello_world'
-
   get '*path',
   to: 'fallback#index',
   constraints: ->(req) { !req.xhr? && req.format.html? }
