9c12484bac1a0991a8978e3b0b2faed7ed459ee8
koltvictor <kolts.email@gmail.com>
2022-02-05 14:45:11 -0800

    welcome page styling

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function Welcome({Image1}) {
     return(
         <div className="welcomeWrapper">
-            <Link to="/home" className="welcome">Enter JKM Gardens</Link><br/>
+            <Link to="/home" className="welcome">Natural Art Gardens</Link><br/>
             <img src={Image1} className="welcomeImg" alt="garden logo" />
         </div>
     )
