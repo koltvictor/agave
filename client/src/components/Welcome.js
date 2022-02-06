a4476a86f45c0ab6fdf21fa30cc9b640c1d0993c
koltvictor <kolts.email@gmail.com>
2022-02-06 11:27:19 -0800

    added css trick on welcome page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,8 +4,9 @@ import {Link} from 'react-router-dom';
 export default function Welcome({Image1}) {
     return(
         <div className="welcomeWrapper">
-            <Link to="/home" className="welcome">Natural Art Gardens</Link><br/>
+            <Link to="/home" className="welcome"></Link><br/>
             <img src={Image1} className="welcomeImg" alt="garden logo" />
+            
         </div>
     )
 }
