7ea4abc058eb82d0807296682ba3c8c27e2e16f0
koltvictor <kolts.email@gmail.com>
2022-02-06 12:26:24 -0800

    added verbiage to home page and image

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import Header from './Header';
-
+import Image3 from '../agavedrawing.png'
 import React from "react";
 
 export default function Home() {
@@ -9,6 +9,9 @@ export default function Home() {
       <br/>
       <h1 className="navHeader">Hello & Welcome</h1>
       <h3 className="navHeader">to our gardens of Agave americana!</h3>
+      <h3 className="navHeader">Please feel free to peruse our gallery, view our agaves, and contact us to set up an appointment.</h3>
+      <br/>
+      <img src={Image3} className="contactImg" alt="garden logo" />
     </div>
   );
 }
